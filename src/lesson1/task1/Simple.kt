0eb83ddcc480141d9096b2c7455f470387a31f60
PhamBinh98 <43148428+PhamBinh98@users.noreply.github.com>
2018-09-25 21:39:57 +0300

    first commit

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson1.task1
 
 import kotlin.math.*
@@ -58,7 +59,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 3600+ minutes* 60 + seconds)
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 3600 + minutes * 60 + seconds)
 
 /**
  * Тривиальная
@@ -67,11 +68,11 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 3600+ minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double  {
-    val a= sagenes * 48
-    val b= arshins * 16
-    val c= vershoks
-    return ((a+b+c)*4.445)/100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val a = sagenes * 48
+    val b = arshins * 16
+    val c = vershoks
+    return ((a + b + c) * 4.445) / 100
 }
 
 /**
@@ -80,7 +81,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double  {
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double{
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double {
     val a = grad
     val b = min / 60.0
     val c = sec / 3600.0
@@ -94,9 +95,9 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double{
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
 fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
-val a = x2-x1
-val b = y2-y1
-return sqrt(sqr(a)+ sqr(b))
+    val a = x2 - x1
+    val b = y2 - y1
+    return sqrt(sqr(a) + sqr(b))
 }
 
 /**
@@ -105,7 +106,7 @@ return sqrt(sqr(a)+ sqr(b))
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = number/100 - ( number/1000)* 10
+fun thirdDigit(number: Int): Int = number / 100 - (number / 1000) * 10
 
 /**
  * Простая
@@ -115,9 +116,9 @@ fun thirdDigit(number: Int): Int = number/100 - ( number/1000)* 10
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
 fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
-val a= hoursArrive * 60 + minutesArrive
-val b= hoursDepart * 60 + minutesDepart
-return a-b
+    val a = hoursArrive * 60 + minutesArrive
+    val b = hoursDepart * 60 + minutesDepart
+    return a - b
 }
 
 /**
@@ -128,8 +129,8 @@ return a-b
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double {
-   val a= Math.pow(1 + percent / 100.0, 3.0)
-    return initial * a
+     return initial * ( Math.pow(1 + percent / 100.0, 3.0))
+
 }
 
 /**
@@ -138,9 +139,9 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int{
-    val a = number/100
-    val b = (number - a*100)/10
-    val c= (number -b*10- a* 100)
-    return c*100 + b*10 + a
+fun numberRevert(number: Int): Int {
+    val a = number / 100
+    val b = (number - a * 100) / 10
+    val c = (number - b * 10 - a * 100)
+    return c * 100 + b * 10 + a
 }
